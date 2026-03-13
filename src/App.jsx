7e0e51e0d35e3f8d7ccedc1993ab6895e99e6700
George Tzagkarakis <geotza77@gmail.com>
2026-03-13 02:46:26 +0200

    Import EnergyCompare component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-
+import EnergyCompare from "./energy/energyCompare"
 
 /* ═══ SHEETJS XLSX EXPORT ═══ */
 const loadXLSX=()=>new Promise((res,rej)=>{
